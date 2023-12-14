13133e6b4d576cd56d26bfaef3926ec1e3fcbedc
willykibet <willykipchumba22@gmail.com>
2023-12-14 12:51:51 +0300

    mh

--- lib/login.py
+++ lib/login.py
@@ -28,3 +28,53 @@ def login_menu():
     elif choice == "3":
         print("Goodbye!")
     else:
+               print("Invalid choice. Please try again")
+
+#enter details to log in as doctor
+def login_as_doctor():
+    username = input("Enter your username:")
+    password = getpass("Enter your password:")
+#validates the inputs from user and compares with the one stored in database
+    doctor = session.query(User).filter(User.username == username, User.password == password, User.role == "doctor").first()
+    if doctor:
+        print(f"Logged in as Doctor: {doctor.username}")
+        doctor_menu(doctor)
+    else:
+        print("Invalid username or password")
+        
+
+#after logging in as a doctor
+def doctor_menu(doctor):
+    print(f"Welcome, Dr. {doctor.username}!")
+    print("1. View Patients")
+    print("2. View Appointments")
+    print("3. Add a Patient")
+    print("4. Delete a Patient")
+    print("5. Get Report")
+    print("6. Update Appointment Status")
+    print("7. Logout")
+#after logging as doctor there are several options made avaibale to them
+    choice = input("Please enter your choice:")
+
+    if choice == "1":
+        read_patients()
+    elif choice == "2":
+        read_appointments()
+    elif choice == "3":
+        add_patient()
+    elif choice == "4":
+        delete_patient()
+    elif choice == "5":
+        get_report()
+    elif choice == "6":
+        update_appointment_status()
+    elif choice == "7":
+        print("Logging out. Goodbye!")
+    else:
+        print("Invalid choice. Please try again")
+        doctor_menu(doctor)
+
+
+#doctor will be able to view the list of patients with the help of tabulate
+def read_patients():
+    patients = session.query(Patient).all()
