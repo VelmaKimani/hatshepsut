2a8c6d631b4865892835dce24eaebb7c5d3e3112
Edith Wanguba <wanguamor@gmail.com>
2023-12-14 14:14:02 +0300

    Update on patients and appointment tables

--- lib/main.py
+++ lib/main.py
@@ -43,3 +43,46 @@ class Appointment(Base):
 
     patient = relationship("Patient", back_populates="appointments")
     doctor = relationship("Doctor", back_populates="appointments")
+
+    def __init__(self, appointment_date, appointment_time, status, appointment_type, patient_id, doctor_id):
+        self.appointment_date = appointment_date
+        self.appointment_time = appointment_time
+        self.status = status
+        self.appointment_type = appointment_type  # Assign the appointment_type
+        self.patient_id = patient_id
+        self.doctor_id = doctor_id
+
+# ...
+#users table
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    password = Column(String)
+    role = Column(String)  # 'doctor' or 'patient'
+
+    def __repr__(self):
+        return f"<User(username='{self.username}', role='{self.role}')>"
+    
+#class patient and its attribute
+class Doctor(Base):
+     #Doctor table and some of its attribute
+     __tablename__ = 'doctors'
+     id= Column(Integer, primary_key=True)
+     name= Column(String)
+     specialization= Column(String)
+     contact_info= Column(String)
+     appointments = relationship("Appointment", back_populates="doctor")
+
+     def __init__(self, name, specialization, contact_info, appointments):
+         self.name=name
+         self.specialization=specialization
+         self.contact_info=contact_info
+        
+
+# Connect to the database
+engine = sqlalchemy.create_engine('sqlite:///linda_mama_care.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+# Create the tables
+Base.metadata.create_all(engine)
